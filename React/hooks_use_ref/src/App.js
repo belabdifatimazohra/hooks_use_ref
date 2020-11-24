c300ceaac7cdbc44f3a7434b76e7215fb066f490
belabdifatimazohra <fatima.zohra.belabdi@gmail.com>
2020-11-24 15:00:07 +0000

    useRef

---
+++ React/hooks_use_ref/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TestUseRef from './TestUseRef'
+
+function App() {
+  return (
+    <div className="App">
+      <TestUseRef />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ React/hooks_use_ref/src/TestUseRef.js
@@ -0,0 +1,31 @@
+import React, { useState, useRef } from 'react'
+
+function TestUseRef() {
+    let [name, setName] = useState("Ned stark");
+    // we declare the input inside the variable
+    let nameRef = useRef();
+    // we are referring to input to change the value
+    const submitButton = () => {
+        setName(nameRef.current.value);
+    };
+
+    return (
+        <div className="useRef">
+            <p>{name}</p>
+            <h1>Who is your favorite Games of throne character</h1>
+
+            <div>
+                <input
+                    placehoder="enter your preferred GOT character..."
+                    ref={nameRef}
+                    type="text"
+                />
+                <button type="button" onClick={submitButton}>
+                    Submit
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default TestUseRef
